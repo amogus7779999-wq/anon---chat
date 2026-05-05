14ead506698bc3440d95331afd8f6d59e50813b7
amogus7779999-wq <amogus7779999@example.com>
2026-05-05 19:36:51 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+
+messages = []
+
+html = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Аноним чат</title>
+    <meta http-equiv="refresh" content="2">
+</head>
+<body>
+    <h2>Общий чат</h2>
+
+    <form method="post">
+        <input type="text" name="name" placeholder="Ник" required>
+        <input type="text" name="msg" placeholder="Сообщение" required>
+        <button type="submit">Отправить</button>
+    </form>
+
+    <hr>
+
+    {% for m in messages %}
+        <p>{{ m }}</p>
+    {% endfor %}
+</body>
+</html>
+"""
+
+@app.route("/", methods=["GET", "POST"])
+def chat():
+    if request.method == "POST":
+        name = request.form.get("name")
+        msg = request.form.get("msg")
+        if name and msg:
+            messages.append(f"{name}: {msg}")
+    return render_template_string(html, messages=messages)
+
+app.run(host="0.0.0.0", port=10000)
